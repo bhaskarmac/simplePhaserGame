aa0b4fa94ee44a9f58638073d93edec9fdf8a489
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-08-30 11:49:07 +0530

    Loading the assets in game

--- js/gameScript.js
+++ js/gameScript.js
@@ -1,6 +1,12 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+
+    game.load.image('sky', '../img/sky.png');
+    game.load.image('ground', '../img/platform.png');
+    game.load.image('star', '../img/star.png');
+    game.load.spritesheet('dude', '../img/dude.png', 32, 48);
+
 }
 
 function create() {
